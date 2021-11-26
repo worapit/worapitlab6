fa2420dc6103aa9beadcd314c384aa8740d89d8d
worapit <drm.wmwork@gmail.com>
2021-11-26 15:13:46 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int N,i = 0,k = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> N;
+    while(N != 0){
+         if(N%2 == 0){
+             i++;
+         }else{
+             k++;
+         }
+         cout << "Enter an integer: ";
+         cin >> N;
+    }
+    cout << "#Even numbers = "<< i << "\n";
+    cout << "#Odd numbers = " << k << "\n";
     return 0;
 }
