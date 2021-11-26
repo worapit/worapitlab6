a9b36b3274b331f97da09728583b3b5b5b938f19
worapit <drm.wmwork@gmail.com>
2021-11-26 16:07:39 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
+
+double deg2rad(double a){
+    a = a*M_PI/180;
+    return a;
+}
+
+double rad2deg(double b){
+    b = b*180/M_PI;
+    return b;
+}
+
+double findXComponent(double L1,double L2,double A1,double A2){
+    double x;
+    x = L1*cos(A1)+L2*cos(A2);
+    return x;
+}
+ 
+double  findYComponent(double L1,double L2,double A1,double A2){
+    double y;
+    y = L1*sin(A1)+L2*sin(A2);
+    return y;
+}
+
+double pythagoras(double x,double y){
+    double R;
+    R = sqrt(pow(x,2)+pow(y,2));
+    return R;
+}
+
+double showResult(double c, double d){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    cout << "Length of the resultant vector = " << c << "\n";
+    cout << "Direction of the resultant vector (deg) = " << d << "\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" << "\n";
+    return c;
+    return d;
+}
+
+
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
