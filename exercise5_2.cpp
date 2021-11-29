cd5104b99e9253e0d2620dbaabee3c08e8dc3d8e
worapit <drm.wmwork@gmail.com>
2021-11-29 21:57:41 +0700

    complete

--- exercise5_2.cpp
+++ exercise5_2.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    string text1, text2;
+    int N, count; 
+
+    cout << "Enter the frist text:";
+    cin >> text1;
+
+    cout << "Enter the second text:";
+    cin >> text2;
+
+    cout << "Enter N:";
+    cin >> N;
+
+    count = 0;
+    while(count < N){
+        if(count%2 == 0){
+            cout << text1 << " " ; 
+        }else{
+            cout << text2 << " " ;
+        }
+        count++;
+    }
+    return 0;
+}
